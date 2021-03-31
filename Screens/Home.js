1001f22b3f5780d2d1143b12452bc31382de8a8a
Azeem Sarwar <as1265513@gmail.com>
2021-03-31 11:53:01 +0500

    Render incoming Expanses
    Expanses Rander

--- Screens/Home.js
+++ Screens/Home.js
@@ -233,8 +233,114 @@ export default function Home() {
       </View>
     );
   }
-  
 
+  function IncomingExpansesTitle(){
+    return(
+      <View style={{padding:SIZES.padding,height: 80, backgroundColor: COLORS.lightGray2}}>
+        <Text style={{color:COLORS.primary,...FONTS.h2}} >Incoming Expanses</Text>
+        <Text style={{color:COLORS.darkgray,...FONTS.body3}}>12 Total</Text>
+      </View>
+
+    )
+  }
+  
+function renderIncomingExpenses(){
+  let allExpenses = selectedCategory ? selectedCategory.expenses : []
+        let incomingExpenses = allExpenses.filter(a => a.status == "P")
+        const renderItem = ({ item, index }) => (
+          <View style={{
+              width: 300,
+              marginRight: SIZES.padding,
+              marginLeft: index == 0 ? SIZES.padding : 0,
+              marginVertical: SIZES.radius,
+              borderRadius: SIZES.radius,
+              backgroundColor: COLORS.white,
+              ...styles.shadow
+          }}>
+              {/* Title */}
+              <View style={{ flexDirection: 'row', padding: SIZES.padding, alignItems: 'center' }}>
+                  <View
+                      style={{
+                          height: 50,
+                          width: 50,
+                          borderRadius: 25,
+                          backgroundColor: COLORS.lightGray,
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          marginRight: SIZES.base
+                      }}
+                  >
+                      <Image
+                          source={selectedCategory.icon}
+                          style={{
+                              width: 30,
+                              height: 30,
+                              tintColor: selectedCategory.color
+                          }}
+                      />
+                  </View>
+
+                  <Text style={{ ...FONTS.h3, color: selectedCategory.color, }}>{selectedCategory.name}</Text>
+              </View>
+
+              {/* Expense Description */}
+              <View style={{ paddingHorizontal: SIZES.padding }}>
+                  {/* Title and description */}
+                  <Text style={{ ...FONTS.h2, }}>{item.title}</Text>
+                  <Text style={{ ...FONTS.body3, flexWrap: 'wrap', color: COLORS.darkgray }}>
+                      {item.description}
+                  </Text>
+
+                  {/* Location */}
+                  <Text style={{ marginTop: SIZES.padding, ...FONTS.h4, }}>Location</Text>
+                  <View style={{ flexDirection: 'row' }}>
+                      <Image
+                          source={icons.pin}
+                          style={{
+                              width: 20,
+                              height: 20,
+                              tintColor: COLORS.darkgray,
+                              marginRight: 5
+                          }}
+                      />
+                      <Text style={{ marginBottom: SIZES.base, color: COLORS.darkgray, ...FONTS.body4 }}>{item.location}</Text>
+                  </View>
+              </View>
+
+              {/* Price */}
+              <TouchableOpacity
+                  style={{
+                      height: 50,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      borderBottomStartRadius: SIZES.radius,
+                      borderBottomEndRadius: SIZES.radius,
+                      backgroundColor: selectedCategory.color,
+                  }}
+              >
+                  <Text style={{ color: COLORS.white, ...FONTS.body3 }}>CONFIRM {item.total.toFixed(2)} USD</Text>
+              </TouchableOpacity>
+          </View>
+      )
+  return(
+    <View>
+      {IncomingExpansesTitle()}
+    {incomingExpenses.length > 0 &&<FlatList
+    data={incomingExpenses}
+    renderItem={renderItem}
+    keyExtractor={item=>`${item.id}`}
+    horizontal
+    showsHorizontalScrollIndicator={true}
+      />}
+       {
+                    incomingExpenses.length == 0 &&
+                    <View style={{ alignItems: 'center', justifyContent: 'center', height: 300 }}>
+                        <Text style={{ color: COLORS.primary, ...FONTS.h3 }}>No Record</Text>
+                    </View>
+                }
+    </View>
+  )
+}
 
   return (
     <View style={{flex: 1, backgroundColor: COLORS.lightGray}}>
@@ -250,7 +356,7 @@ export default function Home() {
                     viewMode == "list" &&
                     <View>
                         {RandersCategoryList()}
-                        {/* {renderIncomingExpenses()} */}
+                        {renderIncomingExpenses()}
                     </View>
                 
       }</ScrollView>
